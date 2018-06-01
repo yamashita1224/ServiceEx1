a3db649fb0331a3605daf6742e7e79dae0b7f1c2
Takuo WATANABE <takuo@acm.org>
2018-06-01 13:09:01 +0900

    First runnable version

--- app/src/main/java/jp/ac/titech/itpro/sdl/serviceex1/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/serviceex1/MainActivity.java
@@ -1,13 +1,42 @@
 package jp.ac.titech.itpro.sdl.serviceex1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private final static String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate in " + Thread.currentThread());
         setContentView(R.layout.activity_main);
+
     }
+
+    public void pressTest1(View v) {
+        testService1();
+    }
+
+    public void pressTest2(View v) {
+        testService2();
+    }
+
+    private void testService1() {
+        Log.d(TAG, "testService1 in " + Thread.currentThread());
+        Intent intent = new Intent(this, TestService1.class);
+        intent.putExtra(TestService1.EXTRA_MYARG, "Hello, Service1");
+        startService(intent);
+    }
+
+    private void testService2() {
+        Log.d(TAG, "testService2 in " + Thread.currentThread());
+        Intent intent = new Intent(this, TestService2.class);
+        intent.putExtra(TestService2.EXTRA_MYARG, "Hello, Service2");
+        startService(intent);
+    }
+
 }
---
+++ app/src/main/java/jp/ac/titech/itpro/sdl/serviceex1/TestService2.java
@@ -0,0 +1,39 @@
+package jp.ac.titech.itpro.sdl.serviceex1;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.util.Log;
+
+public class TestService2 extends IntentService {
+    private final static String TAG = "TestService2";
+    public final static String EXTRA_MYARG = "MYARG";
+
+    TestService2() {
+        super(TAG);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Log.d(TAG, "onHandleIntent in " + Thread.currentThread());
+        Log.d(TAG, "myarg = " + intent.getStringExtra(EXTRA_MYARG));
+        try {
+            Thread.sleep(5000);
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG, "onCreate in " + Thread.currentThread());
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d(TAG, "onDestroy in " + Thread.currentThread());
+        super.onDestroy();
+    }
+}
+
